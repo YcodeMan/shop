c7a021c3d3dfbdbfe8a5b42ddcf14d75426ca49b
pananing <1131542635@qq.com>
2019-05-09 11:18:30 +0800

    完成登陆注册功能v1

--- app/login/login.php
+++ app/login/login.php
@@ -2,10 +2,20 @@
 header('Access-Control-Allow-Origin:*');
 $json_string = file_get_contents('../json/user.json');
 $data = json_decode($json_string, true);
-
-$json_string = json_encode($data);
-print_r($data);
-file_put_contents('../json/user.json', $json_string);
-echo "<script>location.href='./../login/login.html';</script>";        
+$uname=$_GET['username'];
+$flag= true;
+// var_dump($data);die;
+foreach ($data as $key => $val){
+    if ($flag) {
+        if ($uname==$val['username']){
+            $flag=false;
+            $code=200;
+        } else {
+            $code=-1;
+        }
+    }   
+};
+echo $code;die;
+//echo "<script>location.href='./../login/login.html';</script>";        
 
 ?>
