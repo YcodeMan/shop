02a7ba4a665d6afc5cdbca11cdf3acf0950eac5a
YcodeMan <1130578809@qq.com>
2019-05-10 09:21:06 +0800

    modify dist

--- dist/app/content/content.js
+++ dist/app/content/content.js
@@ -6,7 +6,7 @@ $(function () {
         $.each(shopMsg, function (k, v) {
            str += `
                     <li>
-                    <a href="#id=${v.id}">
+                    <a href="./app/shopMsg/shopMsg.html?id=${v.id}">
                         <div class="NewProBlock">
                             <div class="txtC">
                                 <h3>${v.title}<br>${v.type}</h3>
@@ -39,7 +39,7 @@ $(function() {
             mbLeftStr = ``,
             mbrightStr = ``;
         $.each(content, function(k,v) {
-            contentStr += `<li><a href="#id=${v.id}">${v.text}</a></li>`;
+            contentStr += `<li><a href="./app/shopMsg/shopMsg.html?id=${v.id}">${v.text}</a></li>`;
         });
         $.each(mbLeft, function(k,v) {
             var str = '';
@@ -58,7 +58,7 @@ $(function() {
                     str = 'small block-tiem bgwhite';      
             }
             mbLeftStr += `
-            <a href="#id=${v.id}" class="${str}" style="margin:0 5px 10px 5px">
+            <a href="./app/shopMsg/shopMsg.html?id=${v.id}" class="${str}" style="margin:0 5px 10px 5px">
                 <div class="txt">
                     <p class="xinghao">${v.type}</p>
                     <h3 class="miaoshu">${v.title}<br>${v.introduce}</h3>
@@ -94,7 +94,7 @@ $(function() {
                     str = 'small block-tiem bgwhite';      
             }
             mbrightStr += `
-                <a href="#id=${v.id}" class="${str}" style="margin:0 5px 10px 5px">
+                <a href="./app/shopMsg/shopMsg.html?id=${v.id}" class="${str}" style="margin:0 5px 10px 5px">
                     <img src="${v.imgSrc}" alt="" class="touming">
                     <div class="txt">
                         <p class="xinghao">${v.type}</p>
@@ -108,4 +108,45 @@ $(function() {
        $(".mbLeft[class*='left']").append(mbLeftStr);
        $(".mbLeft[class*='right']").append(mbrightStr);
     })
-})
+});
+$(function() {
+    //高度滚动出现
+
+$(window).scroll( 
+    function() {
+        let height=$(document).scrollTop()
+        if(0 <=height && height < 1499){
+            $(".rightFixNav").removeClass('noyincang').addClass('yincang');
+        }else if(height>1500){
+            $(".rightFixNav").removeClass('yincang').addClass('noyincang');
+            if(1600 <= height && height <= 2800){
+             $($(".rightFixNav ul li")[0]).addClass('bgblack').siblings().removeClass('bgblack')
+            } else if(2801 <= height && height <= 4400){
+                $($(".rightFixNav ul li")[1]).addClass('bgblack').siblings().removeClass('bgblack')
+            } else if(4401 <= height && height <= 6000){
+                $($(".rightFixNav ul li")[2]).addClass('bgblack').siblings().removeClass('bgblack')
+            } else if(6001 <= height && height <= 6900){
+                $($(".rightFixNav ul li")[3]).addClass('bgblack').siblings().removeClass('bgblack')
+            } else if(6901 <= height){
+                $($(".rightFixNav ul li")[4]).addClass('bgblack').siblings().removeClass('bgblack')
+            } 
+        } 
+    }
+ );
+ function changeblock(ele){
+     let liAll=$(".rightFixNav ul li");
+     if(liAll[0]==ele){
+        $('html,body').animate({scrollTop:1800}, 400)
+     } else if(liAll[1]==ele){
+        $('html,body').animate({scrollTop:3100}, 400)
+     } else if(liAll[2]==ele){
+        $('html,body').animate({scrollTop:4650}, 400)
+     } else if(liAll[3]==ele){
+        $('html,body').animate({scrollTop:6200}, 400)
+     } else if(liAll[4]==ele){
+        $('html,body').animate({scrollTop:7100}, 400)
+     } else if(liAll[5]==ele){
+        $('html,body').animate({scrollTop:0}, 400)
+     }
+ }
+});
